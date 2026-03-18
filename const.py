a8f151d98dba52cc568e2de6c60adc93376690ec
Letícia Cardoso <leticia24.c.ramos@gmail.com>
2026-03-18 10:38:25 -0300

    corrections to the strings

--- const.py
+++ const.py
@@ -3,10 +3,10 @@ COLOR_ORANGE = (255, 128, 0)
 COLOR_WHITE = (255, 255, 255)
 
 # M
-MENU_OPTION = ('NEW GAME 1P'
-               'NEW GAME 2P - COOPERATIVE'
-               'NEW GAME 2P - COMPETITIVE'
-               'SCORE'
+MENU_OPTION = ('NEW GAME 1P',
+               'NEW GAME 2P - COOPERATIVE',
+               'NEW GAME 2P - COMPETITIVE',
+               'SCORE',
                'EXIT')
 
 # W
