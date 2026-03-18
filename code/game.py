c7c8c5b6169b180148a48fc61885dc7456651fd9
Letícia Cardoso <leticia24.c.ramos@gmail.com>
2026-03-18 10:25:09 -0300

    creating the menu: background, sound and fonts creating the const file

--- code/game.py
+++ code/game.py
@@ -3,20 +3,19 @@
 import pygame
 
 from code.menu import Menu
+from const import WIN_WIDTH, WIN_HEIGHT
+
 
 class Game:
     def __init__(self):
         pygame.init()
-        self.window = pygame.display.set_mode(size=(600, 480))
+        self.window = pygame.display.set_mode(size=(WIN_WIDTH, WIN_HEIGHT))
 
     def run(self):
 
+
         while True:
             menu = Menu(self.window)
+            menu.run()
             pass
-        # check for all events
-            # for event in pygame.event.get():
-             #   if event.type == pygame.QUIT:
-              #      pygame.quit()  # close window
-               #     quit()  # end pygame
 
